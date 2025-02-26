133b42547b2f27271bdb99a3ffd59e41ccfcf19a
윤효진 <hyojinee@dgsw.hs.kr>
2025-02-26 21:44:23 +0900

    주석 삭제

--- express/app.js
+++ express/app.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 const userRouter = require('./user');
 
-app.use('/user', userRouter); // 라우터 객체를 /user 경로에 등록
+app.use('/user', userRouter);
 
 app.get('/', (req, res) => { // 루트 경로에 대한 GET 요청 리스너
     res.send('Hello World!');
